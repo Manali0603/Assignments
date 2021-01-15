31196aa2d6d330995e6462692847c0186b785324
Manali0603 <48184767+Manali0603@users.noreply.github.com>
2021-01-15 10:28:45 +0530

    commit

--- src/com/maersk/java8Practice/streamsEx.java
+++ src/com/maersk/java8Practice/streamsEx.java
@@ -1,9 +1,7 @@
 package com.maersk.java8Practice;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.function.Consumer;
 
 class Employee {
     long id;
@@ -18,16 +16,20 @@ class Employee {
         this.salary = salary;
     }
 
+    public int getId() {
+        return (int) id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     public String getName() {
         return name;
     }
 
     public double getSalary() {
         return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 
     @Override
@@ -42,49 +44,63 @@ class Employee {
 }
 
 public class streamsEx {
-    static double increment(double sal) {
-
-        if (sal * 12 < 200000)
-            sal = sal + (.15 * sal);
-
-        else
-            sal = sal + (.05 * sal);
-
-        return sal;
-    }
-
     public static void main(String[] args) {
 
         List<Employee> employees = Arrays.asList(new Employee(91, "Zoya", 22, 10000),
                 new Employee(96, "Siri", 23, 18000),
                 new Employee(99, "Ankita", 21, 15000),
-                new Employee(90, "Manali", 25, 20000));
+                new Employee(90, "Manali", 25, 20000),
+                new Employee(98, "Manali", 22, 15000),
+                new Employee(94, "Siri", 22, 15000));
+
+        Consumer<List<Employee>> empSalIncrement = list -> {
+            for (int i = 0; i < employees.size(); i++) {
+                double sal = list.get(i).getSalary();
+                if (sal * 12 < 200000)
+                    sal += sal * 0.15;
+                else
+                    sal += sal * 0.05;
+                Employee e = new Employee(list.get(i).getId(), list.get(i).getName(), list.get(i).getAge(), sal);
+                list.set(i, e);
+            }
+        };
+
+        //sorting by name
+        Consumer<List<Employee>> printEmployees = list -> list
+                .stream()
+                .sorted(Comparator.comparing(Employee::getName))
+                .forEach(e -> System.out.println("SORTING BY NAME : " + e));
+        //sorting by salary
+        Consumer<List<Employee>> printEmployees1 = list -> list
+                .stream()
+                .sorted((Employee e1, Employee e2) -> (int) (e1.getSalary() - e2.getSalary()))
+                .forEach(e -> System.out.println("SORTING BY SALARY : " + e));
 
 
-        List<Employee> ls = employees.stream().map(e -> {
-            double sal = streamsEx.increment(e.salary);
-            e.setSalary(sal);
-            return e;
-        }).collect(Collectors.toList());
+        //counting the occurrence of same names
+        Consumer<List<Employee>> countOccurance;
+        countOccurance = c -> {
+            Map<String, Integer> count = new HashMap<>();
+            for (Employee employee : employees) {
+                if (count.containsKey(employee.getName())) {
+                    String keyName = employee.getName();
+                    int valCount = count.get(keyName);
+                    count.put(keyName, ++valCount);
+                } else {
+                    int j = 0;
+                    count.put(employee.getName(), ++j);
+                }
+            }
+            System.out.println(count);
+        };
 
 
-        //sort based on names
-        Comparator<Employee> compareByName = Comparator
-                .comparing(Employee::getName);
+        //printing according to sorted names and sorted salaries
+        empSalIncrement.andThen(printEmployees).andThen(printEmployees1).accept(employees);
 
-        List<Employee> sortedEmployees = employees.stream()
-                .sorted(compareByName)
-                .collect(Collectors.toList());
-        System.out.println("Based on Names : \n" + sortedEmployees);
-
-        //sort based on incremented salary
-        Comparator<Employee> compareBySalary = Comparator
-                .comparing(Employee::getSalary);
-
-        List<Employee> sortedEmployees1 = employees.stream()
-                .sorted(compareBySalary)
-                .collect(Collectors.toList());
-        System.out.println("Based on Salary : \n" + sortedEmployees1);
+        //printing the occurrence of names
+        System.out.println("Printing the occurrence of names :");
+        countOccurance.accept(employees);
 
 
     }
