1ecfdd872d29e4aebefae37624a74b528f182d98
Manali0603 <48184767+Manali0603@users.noreply.github.com>
2021-02-04 23:14:46 +0530

    Commit2

--- src/com/maersk/jdbcAssignment1.java
+++ src/com/maersk/jdbcAssignment1.java
@@ -10,42 +10,42 @@ public class jdbcAssignment1 {
         //con.setAutoCommit(false);
 
         Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("Select * from EmployeeTable");
+        ResultSet emp_details = stmt.executeQuery("Select * from EmployeeTable");
         System.out.println("....DETAILS OF EMPLOYEES....");
-        while (rs.next())
-            System.out.println(rs.getInt(1) + "   " + rs.getString(2) + "      " + rs.getInt(3) +
-                    "   " + rs.getInt(4));
+        while (emp_details.next())
+            System.out.println(emp_details.getInt(1) + "   " + emp_details.getString(2) + "      " + emp_details.getInt(3) +
+                    "   " + emp_details.getInt(4));
 
         /*ResultSet rs1 = stmt.executeQuery("SELECT dept_name AS 'Dept_Name', COUNT(*) AS 'Total Employees' FROM EmployeeTable GROUP BY emp_dept");*/
 
         //NUMBER OF EMPLOYEES IN A DEPARTMENT
         System.out.println("\n....NUMBER OF EMPLOYEES IN A DEPARTMENT....\n");
-        ResultSet rs1 = stmt.executeQuery("SELECT e2.emp_dept, d2.dept_name,count(*) \n" +
+        ResultSet empCountDept = stmt.executeQuery("SELECT e2.emp_dept, d2.dept_name,count(*) \n" +
                 "     FROM employeetable e2  \n" +
                 "      INNER JOIN departmenttable d2 \n" +
                 "        ON e2.emp_dept = d2.dept_id group by e2.emp_dept,d2.dept_id ");
-        while (rs1.next()) {
-            int count_emp = rs1.getInt("count");
-            System.out.println(rs1.getInt("emp_dept") + "   " + rs1.getString("dept_name") + "   " + count_emp);
+        while (empCountDept.next()) {
+            int count_emp = empCountDept.getInt("count");
+            System.out.println(empCountDept.getInt("emp_dept") + "   " + empCountDept.getString("dept_name") + "   " + count_emp);
         }
 
 
         //GETTING NAMES OF ALL THE DEPARTMENTS
         System.out.println("\n.....DEPARTMENT NAMES.....\n");
-        ResultSet rs2 = stmt.executeQuery("SELECT Dept_name from DepartmentTable");
-        while (rs2.next())
-            System.out.println(rs2.getString(1));
+        ResultSet deptNames = stmt.executeQuery("SELECT Dept_name from DepartmentTable");
+        while (deptNames.next())
+            System.out.println(deptNames.getString(1));
 
 
         //GET NUMBER OF EMPLOYEES WORKING IN COMPANIES
         System.out.println("\n....NUMBER OF EMPLOYEES IN A COMPANY....\n");
-        ResultSet rs3 = stmt.executeQuery("SELECT e2.emp_Company as \"Company_ID\", c2.name as \"Company_Name\",count(*) as \"Emp_Count\"\n" +
+        ResultSet empCountCompany = stmt.executeQuery("SELECT e2.emp_Company as \"Company_ID\", c2.name as \"Company_Name\",count(*) as \"Emp_Count\"\n" +
                 "                     FROM employeetable e2  \n" +
                 "                      INNER JOIN companytable c2\n" +
                 "                        ON e2.emp_company = c2.comid group by e2.emp_company,c2.comid ");
-        while (rs3.next()) {
-            int count_emp = rs3.getInt("Emp_Count");
-            System.out.println(rs3.getInt("Company_ID") + "   " + rs3.getString("Company_Name") + "   " + count_emp);
+        while (empCountCompany.next()) {
+            int count_emp = empCountCompany.getInt("Emp_Count");
+            System.out.println(empCountCompany.getInt("Company_ID") + "   " + empCountCompany.getString("Company_Name") + "   " + count_emp);
         }
 
 
