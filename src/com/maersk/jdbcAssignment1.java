d406626eb6fc1e877fe99fb77cf8937a629ffdf9
Manali0603 <48184767+Manali0603@users.noreply.github.com>
2021-02-05 17:33:51 +0530

    Commit final

--- src/com/maersk/jdbcAssignment1.java
+++ src/com/maersk/jdbcAssignment1.java
@@ -7,14 +7,26 @@ public class jdbcAssignment1 {
 
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Mamata@1769");
-        //con.setAutoCommit(false);
+        con.setAutoCommit(false);
 
         Statement stmt = con.createStatement();
         ResultSet emp_details = stmt.executeQuery("Select * from EmployeeTable");
-        System.out.println("....DETAILS OF EMPLOYEES....");
-        while (emp_details.next())
-            System.out.println(emp_details.getInt(1) + "   " + emp_details.getString(2) + "      " + emp_details.getInt(3) +
-                    "   " + emp_details.getInt(4));
+        ResultSetMetaData rsmd = emp_details.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+
+        //DETAILS OF THE EMPLOYEES
+        System.out.println("\n....DETAILS OF EMPLOYEES....\n");
+//        while (emp_details.next())
+//            System.out.println(emp_details.getInt(1) + "   " + emp_details.getString(2) + "      " + emp_details.getInt(3) +
+//                    "   " + emp_details.getInt(4));
+        while (emp_details.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) System.out.print(",             ");
+                String columnValue = emp_details.getString(i);
+                System.out.print(columnValue + " " + rsmd.getColumnName(i));
+            }
+            System.out.println("");
+        }
 
         /*ResultSet rs1 = stmt.executeQuery("SELECT dept_name AS 'Dept_Name', COUNT(*) AS 'Total Employees' FROM EmployeeTable GROUP BY emp_dept");*/
 
@@ -26,7 +38,25 @@ public class jdbcAssignment1 {
                 "        ON e2.emp_dept = d2.dept_id group by e2.emp_dept,d2.dept_id ");
         while (empCountDept.next()) {
             int count_emp = empCountDept.getInt("count");
-            System.out.println(empCountDept.getInt("emp_dept") + "   " + empCountDept.getString("dept_name") + "   " + count_emp);
+            System.out.println(
+                    empCountDept.getInt("emp_dept") + "          " +
+                            empCountDept.getString("dept_name") + "          " +
+                            count_emp);
+        }
+
+        //DETAILS OF EMPLOYEES IN A PARTICULAR DEPARTMENT
+        System.out.println("\n....EMPLOYEE DETAILS IN A DEPARTMENT....\n");
+        ResultSet empDetailsDept = stmt.executeQuery("SELECT e2.emp_id ,e2.emp_name ,e2.emp_city, d2.dept_name,c2.name \n" +
+                "     FROM((employeetable e2  \n" +
+                "      INNER JOIN departmenttable d2 ON e2.emp_dept = d2.dept_id)\n" +
+                "      inner join companytable c2 on e2.emp_company =c2.comid) where e2.emp_dept =923;          ");
+        while (empDetailsDept.next()) {
+            System.out.println(
+                    empDetailsDept.getInt("emp_id") + "          " +
+                            empDetailsDept.getString("emp_name") + "          " +
+                            empDetailsDept.getString("emp_city") + "          " +
+                            empDetailsDept.getString("dept_name") + "          " +
+                            empDetailsDept.getString("name"));
         }
 
 
@@ -45,21 +75,29 @@ public class jdbcAssignment1 {
                 "                        ON e2.emp_company = c2.comid group by e2.emp_company,c2.comid ");
         while (empCountCompany.next()) {
             int count_emp = empCountCompany.getInt("Emp_Count");
-            System.out.println(empCountCompany.getInt("Company_ID") + "   " + empCountCompany.getString("Company_Name") + "   " + count_emp);
+            System.out.println(
+                    empCountCompany.getInt("Company_ID") + "          " +
+                            empCountCompany.getString("Company_Name") + "          " +
+                            count_emp);
         }
 
 
         System.out.println("\n....EMPLOYEE DETAILS AFTER UPDATE....\n");
         stmt.executeUpdate("update EmployeeTable set emp_dept =941 where Emp_ID > 119; ");
-        ResultSet updated_table = stmt.executeQuery("SELECT e2.emp_id ,e2.emp_name , d2.dept_name,c2.name \n" +
+        ResultSet updated_table = stmt.executeQuery("SELECT e2.emp_id ,e2.emp_name ,e2.emp_city, d2.dept_name,c2.name \n" +
                 "     FROM((employeetable e2  \n" +
                 "      INNER JOIN departmenttable d2 ON e2.emp_dept = d2.dept_id)\n" +
                 "      inner join companytable c2 on e2.emp_company =c2.comid) ;");
         while (updated_table.next()) {
-            System.out.println(updated_table.getInt("emp_id") + "   " + updated_table.getString("emp_name") + "   " +
-                    updated_table.getString("dept_name") + "   " + updated_table.getString("name"));
+            System.out.println(
+                    updated_table.getInt("emp_id") + "          " +
+                            updated_table.getString("emp_name") + "          " +
+                            updated_table.getString("emp_city") + "          " +
+                            updated_table.getString("dept_name") + "          " +
+                            updated_table.getString("name"));
         }
 
+        con.commit();
         con.close();
     }
 }
